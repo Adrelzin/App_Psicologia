dba54af994b41ef9706a5ca3f8a56241f97a8922
Adrelzin <adrelwilion@gmail.com>
2025-08-13 19:02:48 -0300

    .

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import flask, sqlite3
 
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    with sqlite3.connect('database.db') as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS psicologos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                profissao TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
